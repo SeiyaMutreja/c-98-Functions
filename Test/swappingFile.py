7bd1c432e7bf45b96b67a1e19e2cf12c3681cd0c
SeiyaMutreja <73780380+SeiyaMutreja@users.noreply.github.com>
2021-06-14 15:37:53 +0530

    Add files via upload

---
+++ Test/swappingFile.py
@@ -0,0 +1,11 @@
+def swapFileData:
+    a = open("sample1.txt")
+    b = open("sample2.txt")
+    a.read()
+    b.read()
+    with open(sample1, 'r') as a:
+        data_a = a.read
+    with open(file 1, 'w') as a:
+        a.write(data_b)
+
+swapFileData()
